fb7f103e508082863c5d80fa3c9f44cfe263ccca
jacobe911 <axmuzza@gmail.com>
2017-10-08 23:04:33 +0100

    changed base url

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/a1stocks';
+$config['base_url'] = 'http://localhost/a_a1stocks';
 
 /*
 |--------------------------------------------------------------------------
